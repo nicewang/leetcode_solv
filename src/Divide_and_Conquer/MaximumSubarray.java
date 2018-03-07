f25ba789bea966e2ad29e3ff1eb4b26b81bdf4c3
王晓楠 <15051965381@163.com>
2018-03-07 10:58:48 +0800

    Maximum Subarray - Description

--- src/Divide_and_Conquer/MaximumSubarray.java
+++ src/Divide_and_Conquer/MaximumSubarray.java
@@ -1,5 +1,10 @@
 package Divide_and_Conquer;
 
+/**
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ * For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+ * the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+ */
 public class MaximumSubarray {
 	
 	public static void main(String[] args) {
