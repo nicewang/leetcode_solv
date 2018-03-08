48d01ad92a1a709d5620329927536ba61b1ecbfd
王晓楠 <15051965381@163.com>
2018-03-08 16:51:30 +0800

    description of BalancedBianryTree and PathSum

--- src/Depth_first_Search/BalancedBinaryTree.java
+++ src/Depth_first_Search/BalancedBinaryTree.java
@@ -1,7 +1,59 @@
 package Depth_first_Search;
 
+/**
+ * Given a binary tree, determine if it is height-balanced.
+ * For this problem, a height-balanced binary tree is defined as:
+ * a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ * Example 1:
+ * Given the following tree [3,9,20,null,null,15,7]:
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * Return true.
+ * Example 2:
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ *        1
+ *       / \
+ *      2   2
+ *     / \
+ *    3   3
+ *   / \
+ *  4   4
+ * Return false.
+ */
 public class BalancedBinaryTree {
 	
+	/**
+	 * simpler?
+	 * 用时21ms
+	 * @param root
+	 * @return
+	 */
+	public boolean isBalanced_simpler(TreeNode root) {
+		if(root == null)
+			return true;
+		// 注意，这里用的是高度而不是深度
+		int height = getHeight(root);
+		return height!=-1;
+	}
+	
+	private int getHeight(TreeNode root) {
+		// TODO Auto-generated method stub
+		if(root == null)
+			return 0;
+		int left = getHeight(root.left);
+		int right = getHeight(root.right);
+		if(left == -1)
+			return -1;
+		if(right == -1)
+			return -1;
+		if(left-right > 1 || right-left>1)
+			return -1;
+		return Math.max(getHeight(root.left), getHeight(root.right))+1;
+	}
+
 	/**
 	 * 运行时间3ms
 	 * @param root
@@ -44,6 +96,28 @@ public class BalancedBinaryTree {
 			return getDepth(root.left,i+1);
 		else
 			return Math.max(getDepth(root.left,i+1), getDepth(root.right,i+1));
+	}
+	
+	/**
+	 * 耗时2ms
+	 * @param root
+	 * @return
+	 */
+	public boolean isBalanced_solv3(TreeNode root) {
+		if(root == null)
+			return true;
+		int left = getDepth(root.left);
+		int right = getDepth(root.right);
+		if(left-right > 1 || right-left > 1)
+			return false;
+		return isBalanced_solv3(root.left) && isBalanced_solv3(root.right);
+	}
+
+	private int getDepth(TreeNode root) {
+		// TODO Auto-generated method stub
+		if(root == null)
+			return 0;
+		return Math.max(getDepth(root.left), getDepth(root.right))+1;
 	}
 
 }
--- src/Depth_first_Search/PathSum.java
+++ src/Depth_first_Search/PathSum.java
@@ -4,6 +4,20 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
+/**
+ * Given a binary tree and a sum, 
+ * determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ * For example:
+ * Given the below binary tree and sum = 22,
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ * return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ */
 public class PathSum {
 	
 	/**
