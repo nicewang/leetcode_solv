55a9704c1556fba51aa8388ef4fcf17587e47fe4
王晓楠 <15051965381@163.com>
2018-05-12 12:03:32 +0800

    toutiao 2018 Spring lat

--- src/Try/Try.java
+++ src/Try/Try.java
@@ -5,19 +5,16 @@ import java.util.Scanner;
 public class Try {
 	
 	public static void main(String[] args) {
-		
 		Scanner in = new Scanner(System.in);
 		while(in.hasNext()) {
 			int n = in.nextInt();
 			int[] data = new int[n];
-			int i;
-			for(i = 0; i < n; i++)
+			for(int i = 0; i < n; i++)
 				data[i] = in.nextInt();
-			while(i > 0)
-				System.out.println(data[n-(i--)]);
+			for(int j = 0; j < n; j++)
+				System.out.println(data[j]);
 		}
 		in.close();
-				
 	}
 	
 }
