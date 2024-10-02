edee35d202cd32c1a9880fc8f36dd86d10b1fd00
nicewang <15051965381@163.com>
2024-10-02 23:56:23 +0800

    cmt-20241002-1: +solve problem 1870
    Solve Problem 1870

---
+++ leetcode_solv_cpp/1870.minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=1870 lang=cpp
+ *
+ * [1870] Minimum Speed to Arrive on Time
+ */
+
+// @lc code=star
+#include <vector>
+#include <cmath>
+
+class Solution {
+public:
+    int minSpeedOnTime(std::vector<int>& dist, double hour) {
+        int n = dist.size();
+        long long hr = llround(hour * 100);
+        if (hr <= (n - 1) * 100){
+            return -1;
+        }
+        int l = 1;
+        int r = 1e7;
+        while (l < r){
+            int mid = l + (r - l) / 2;
+            long long t = 0;
+            for (int i = 0; i < n - 1; ++i){
+                t += (dist[i] - 1) / mid + 1;
+            }
+            t *= mid;
+            t += dist[n-1];
+            if (t * 100 <= hr * mid){   
+                r = mid;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return l;   
+    }
+};
+// @lc code=end
+
