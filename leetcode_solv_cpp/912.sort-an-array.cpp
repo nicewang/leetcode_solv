659fa3913003ee5c2f92973ec99d24b37515cac0
nicewang <15051965381@163.com>
2024-10-05 23:14:28 +0800

    cmt-20241005-1: 912 + Heap Sort
    912: + Heap Sort

--- leetcode_solv_cpp/912.sort-an-array.cpp
+++ leetcode_solv_cpp/912.sort-an-array.cpp
@@ -10,8 +10,8 @@
 
 class Solution {
     // Quick Sort 1: exceed time limit
-    int partition_1(std::vector<int>& nums, int l, int r, int pivot) {
-        std::swap(nums[pivot], nums[r]);
+    int partition_1(std::vector<int>& nums, int l, int r, int pivot_idx) {
+        std::swap(nums[pivot_idx], nums[r]);
         int i = l, j = r-1;
         while (i <= j) {
             if (nums[i] > nums[r]) {
@@ -26,8 +26,8 @@ class Solution {
     }
 
     // Quick Sort 2: exceed time limit
-    int partition_2(std::vector<int>& nums, int l, int r, int pivot) {
-        std::swap(nums[pivot], nums[r]);
+    int partition_2(std::vector<int>& nums, int l, int r, int pivot_idx) {
+        std::swap(nums[pivot_idx], nums[r]);
         int i = l-1, j = l;
         while (j < r) {
             if (nums[j] <= nums[r]) {
@@ -63,7 +63,7 @@ class Solution {
         if (l >= r) {
             return;
         }
-        // int pivot = rand()%(r-l+1) + l;
+        // int pivot_idx = rand()%(r-l+1) + l;
         int pivot = nums[l+rand()%(r-l+1)];
         int m = partition(nums, l, r, pivot);
         quick_sort(nums, l, m-1);
@@ -100,12 +100,49 @@ class Solution {
         }
     }
 
+    void max_heapify(std::vector<int>& nums, int cur, int end) {
+        while ((cur << 1) + 1 <= end) {
+            int l = (cur << 1) + 1, r = (cur << 1) + 2;
+            int large_idx = cur;
+            if (nums[l] > nums[cur]) {
+                large_idx = l;
+            }
+            if (r <= end && nums[r] > nums[large_idx]) {
+                large_idx = r;
+            }
+            if (large_idx != cur) {
+                std::swap(nums[cur], nums[large_idx]);
+                // since the leaf had been changed, need to max heapify the sub-heap again
+                cur = large_idx;
+            } else {
+                break; // Attention: important
+            }
+        }
+    }
+
+    void build_maxHeap(std::vector<int>& nums, int end) {
+        for (int i = end/2; i >= 0; i--) {
+            max_heapify(nums, i, end);
+        }
+    }
+
+    // Heap Sort
+    void heap_sort(std::vector<int>& nums) {
+        int n = nums.size();
+        build_maxHeap(nums, n-1);
+        for (int i = n-1; i > 0; i--) {
+            std::swap(nums[0], nums[i]);
+            max_heapify(nums, 0, i-1); // in order to time complexity to o(nlogn) (rather than o((nlog)^2))
+        }
+    }
+
 public:
     std::vector<int> sortArray(std::vector<int>& nums) {
         int n = nums.size();
-        // quick_sort(nums, 0, n-1);
-        tmp.resize(n, 0);
-        merge_sort(nums, 0, n-1);
+        quick_sort(nums, 0, n-1);
+        // tmp.resize(n, 0);
+        // merge_sort(nums, 0, n-1);
+        // heap_sort(nums);
         return nums;
     }
 };
