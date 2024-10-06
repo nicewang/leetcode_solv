500fcc0a8fce73651def987d40428003551ec7de
nicewang <15051965381@163.com>
2024-10-07 02:46:25 +0800

    cmt-20241006-1: +solve problem 871
    Solve Problem 871

---
+++ leetcode_solv_cpp/871.minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=871 lang=cpp
+ *
+ * [871] Minimum Number of Refueling Stops
+ */
+
+// @lc code=start
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    // Way 1: Dynamic Programming
+    int minRefuelStops(int target, int startFuel, std::vector<std::vector<int>>& stations) {
+        int n = stations.size();
+        std::vector<long> dp(n + 1);
+        dp[0] = startFuel;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (dp[j] >= stations[i][0]) {
+                    dp[j + 1] = std::max(dp[j + 1], dp[j] + stations[i][1]);
+                }
+            }
+        }
+        for (int i = 0; i <= n; i++) {
+            if (dp[i] >= target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
