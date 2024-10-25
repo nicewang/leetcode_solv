aaa201dd322fac5851dcb16204978dec933bf8b9
nicewang <15051965381@163.com>
2024-10-26 00:46:08 +0800

    cmt-20241025-2

--- leetcode_solv_cpp/3180.maximum-total-reward-using-operations-i.cpp
+++ leetcode_solv_cpp/3180.maximum-total-reward-using-operations-i.cpp
@@ -11,20 +11,26 @@ class Solution {
 public:
     int maxTotalReward(std::vector<int>& rewardValues) {
         sort(rewardValues.begin(), rewardValues.end());
+        // m: max_reward_val
         int m = rewardValues.back();
-        std::vector<int> dp(2 * m);
-        dp[0] = 1;
-        for (int x : rewardValues) {
-            for (int k = 2 * x - 1; k >= x; k--) {
-                if (dp[k - x]) {
-                    dp[k] = 1;
-                }
-            }
-        }
         int res = 0;
-        for (int i = 0; i < dp.size(); i++) {
-            if (dp[i]) {
-                res = i;
+        // dp[i]: 
+        // True: "sum(rewardVals) == i" exists
+        // Greedly consider we pick the max_reward_val m, the pre_sum < m
+        // So max_possible_reward_sum = pre_sum + m < 2m
+        // So dp[i] range from dp[0] to dp[2m-1]
+        std::vector<bool> dp(2*m);
+        // "reward_sum == 0" is possible
+        dp[0] = true;
+        for (int x : rewardValues) {
+            for (int reward_sum = 2*x-1; reward_sum >= x; reward_sum--) {
+                // Since:
+                // 0 =< pre_sum < x -> x =< reward_sum(=pre+x) < 2x
+                if (dp[reward_sum-x]) {
+                    // pre(=reward_sum-x) exists
+                    dp[reward_sum] = true;
+                    res = std::max(res, reward_sum);
+                }
             }
         }
         return res;
