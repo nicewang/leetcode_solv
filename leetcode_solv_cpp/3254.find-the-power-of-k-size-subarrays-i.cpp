c7027a7dfb545385069ac89a9ec08cc4f1c9ae51
nicewang <15051965381@163.com>
2024-11-06 23:58:04 +0800

    cmt-202411106-1: +solve problem 3254
    Solve Problem 3254

---
+++ leetcode_solv_cpp/3254.find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=3254 lang=cpp
+ *
+ * [3254] Find the Power of K-Size Subarrays I
+ */
+
+// @lc code=start
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        int cnt = 0;
+        vector<int> ans(n - k + 1, -1);
+        for (int i = 0; i < n; i++) {
+            cnt = i == 0 || nums[i] - nums[i - 1] != 1 ? 1 : cnt + 1;
+            if (cnt >= k) {
+                ans[i - k + 1] = nums[i];
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
