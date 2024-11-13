24c76682b02fba31f9b848965545f4f544fae4f0
nicewang <15051965381@163.com>
2024-11-13 23:57:52 +0800

    cmt-20241113-1: +solve problem 3281
    Solve Problem 3281

---
+++ leetcode_solv_cpp/3281.maximize-score-of-numbers-in-ranges.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=3281 lang=cpp
+ *
+ * [3281] Maximize Score of Numbers in Ranges
+ */
+
+// @lc code=start
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+using std::min;
+
+class Solution {
+public:
+    vector<long long> countKConstraintSubstrings(string s, int k, vector<vector<int>>& queries) {
+        int n = s.size();
+        vector<int> count(2, 0);
+        vector<int> right(n, n);
+        vector<long long> prefix(n + 1, 0);
+        int i = 0;
+        for (int j = 0; j < n; ++j) {
+            count[s[j] - '0']++;
+            while (count[0] > k && count[1] > k) {
+                count[s[i] - '0']--;
+                right[i] = j;
+                i++;
+            }
+            prefix[j + 1] = prefix[j] + j - i + 1;
+        }
+
+        vector<long long> res;
+        for (auto& query : queries) {
+            int l = query[0], r = query[1];
+            int i = min(right[l], r + 1);
+            long long part1 = 1LL * (i - l + 1) * (i - l) / 2;
+            long long part2 = prefix[r + 1] - prefix[i];
+            res.push_back(part1 + part2);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
