98df091951af189aeecffac2cfabbe81e0b46ab4
nicewang <15051965381@163.com>
2024-11-15 23:55:50 +0800

    cmt-20241115-1: +solve problem 3239
    Solve Problem 3239

---
+++ leetcode_solv_cpp/3239.minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=3239 lang=cpp
+ *
+ * [3239] Minimum Number of Flips to Make Binary Grid Palindromic I
+ */
+
+// @lc code=start
+#include <vector>
+
+using std::vector;
+using std::min;
+
+class Solution {
+public:
+    int minFlips(vector<vector<int>>& grid) {
+        int rowCnt = 0, colCnt = 0;
+        int m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j1 = 0, j2 = n - 1; j1 < j2; j1++, j2--) {
+                if (grid[i][j1] ^ grid[i][j2]) {
+                    rowCnt++;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            for (int i1 = 0, i2 = m - 1; i1 < i2; i1++, i2--) {
+                if (grid[i1][j] ^ grid[i2][j]) {
+                    colCnt++;
+                }
+            }
+        }
+        return min(colCnt, rowCnt);
+    }
+};
+// @lc code=end
+
